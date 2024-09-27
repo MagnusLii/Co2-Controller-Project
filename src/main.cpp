01e535f9c922d6b01d7bbb90b34be71f2347cee9
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-09-27 05:28:27 +0300

    Short demo of sending/receiving messages

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,42 @@
 #include "PicoOsUart.h"
 #include "ssd1306.h"
 #include "hardware/timer.h"
+#include "connection_handler.h"
+#include "IPStack.h"
+#include <read_runtime_ctr.cpp>
+#include "connection_defines.c"
+#include "api_tasks.h"
+
+
+
+void test_task(void *param) {
+
+    for(;;) {
+        printf("test task\n");
+        vTaskDelay(pdMS_TO_TICKS(10000));
+    }
+}
 
 int main() {
     stdio_init_all();
-    printf("Boot\n");
+    
+    sendQueue = xQueueCreate(SEND_QUEUE_SIZE, sizeof(Message));
+    receiveQueue = xQueueCreate(RECEIVE_QUEUE_SIZE, sizeof(Message));
 
-    for(;;) {
-        printf("It builds!\n");
-        sleep_ms(1000);
+    // IPStack ipstack(WIFI_SSID, WIFI_PASSWORD);
+    ConnectionHandler connHandler = ConnectionHandler();
+
+    xTaskCreate(send_data_task, "send", 1024, (void *) &connHandler, tskIDLE_PRIORITY + 1, nullptr);
+    xTaskCreate(receive_data_task, "receive", 1024, (void *) &connHandler, tskIDLE_PRIORITY + 1, nullptr);
+    xTaskCreate(generate_test_data_task, "generate", 1024, nullptr, tskIDLE_PRIORITY + 1, nullptr);
+    xTaskCreate(initialize_IPStack_task, "init", 1024, (void *) &connHandler, tskIDLE_PRIORITY + 2, nullptr);
+
+    // xTaskCreate(test_task, "test", 4096, nullptr, tskIDLE_PRIORITY + 1, nullptr);
+    vTaskStartScheduler();
+
+    for(;;){
+        printf("shit is wrong\n");
     }
+
     return 0;
 }
