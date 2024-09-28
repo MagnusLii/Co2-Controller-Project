2f691113448e15a5736d753c6106a6a85ed3052b
rsth <riku.helastera@outlook.com>
2024-09-28 21:46:30 +0300

    pointer issues

--- src/RegisterHandler.cpp
+++ src/RegisterHandler.cpp
@@ -1,6 +1,7 @@
 #include "RegisterHandler.h"
 #include "modbus_register.h"
 
+#include <climits>
 #include <iostream>
 #include <ostream>
 
@@ -9,23 +10,29 @@ void ReadRegisterHandler::add_subscriber(QueueHandle_t subscriber) {
 }
 
 void ReadRegisterHandler::remove_subscriber(QueueHandle_t subscriber) {
-    // Not sure if we really need this
     subscribers.erase(std::remove(subscribers.begin(), subscribers.end(), subscriber),
                       subscribers.end());
 }
 
 void ReadRegisterHandler::send_reading() {
-    //std::cout << "Sending reading..." << std::endl;
+    // std::cout << "Sending reading..." << std::endl;
     get_reading();
     for (auto subscriber : subscribers) {
         xQueueSend(subscriber, &reading, 0);
     }
 }
 
-ReadingType ReadRegisterHandler::get_type() {
-    return reading.type;
-}
+ReadingType ReadRegisterHandler::get_type() { return reading.type; }
 
+ModbusReadHandler::ModbusReadHandler(ReadRegister& register_, ReadingType type, std::string name) : reg(register_) {
+    this->reading.type = type;
+    this->name = name;
+
+    xTaskCreate(mb_read_task, name.c_str(), 512, this, tskIDLE_PRIORITY + 1, NULL);
+    this->send_timer = xTimerCreate(name.c_str(), pdMS_TO_TICKS(send_interval), pdTRUE, this,
+                                    send_reading_timer_callback);
+}
+/*
 ModbusReadHandler::ModbusReadHandler(shared_modbus client, uint8_t device_address,
                                      uint16_t register_address, uint8_t nr_of_registers,
                                      bool holding_register, ReadingType type, std::string name)
@@ -33,19 +40,48 @@ ModbusReadHandler::ModbusReadHandler(shared_modbus client, uint8_t device_addres
     this->reading.type = type;
     this->name = name;
 
-    xTaskCreate(mb_read_task, name.c_str(), 256, this, tskIDLE_PRIORITY + 1, NULL);
-    this->send_timer = xTimerCreate(name.c_str(), pdMS_TO_TICKS(send_interval), pdTRUE, this, send_reading_timer_callback);
+    xTaskCreate(mb_read_task, name.c_str(), 512, this, tskIDLE_PRIORITY + 1, NULL);
+    this->send_timer = xTimerCreate(name.c_str(), pdMS_TO_TICKS(send_interval), pdTRUE, this,
+                                    send_reading_timer_callback);
 }
-
-void ModbusReadHandler::get_reading() { reading.value.f32 = reg.get_float(); }
+*/
+void ModbusReadHandler::get_reading() {
+    reading.value.f32 = reg.get_float();
+    //std::cout << reading.value.f32 << std::endl;
+}
 
 void ModbusReadHandler::mb_read() {
     if (send_timer != nullptr) xTimerStart(send_timer, 0);
     for (;;) {
-        TickType_t start = xTaskGetTickCount();
-        while (controller->isbusy()) vTaskDelay(5);
-        //std::cout << "Starting read..." << std::endl;
+        // TickType_t start = xTaskGetTickCount();
+        while (controller->isbusy())
+            vTaskDelay(5);
+        // std::cout << "Starting read..." << std::endl;
         reg.start_transfer();
         vTaskDelay(pdMS_TO_TICKS(reading_interval)); // TBD
+    }
+}
+
+TaskHandle_t WriteRegisterHandler::get_write_task_handle() { return write_task_handle; }
+
+ModbusWriteHandler::ModbusWriteHandler(shared_modbus client, uint8_t device_address,
+                                       uint16_t register_address, uint8_t nr_of_registers,
+                                       ReadingType type, std::string name)
+    : reg(client, device_address, register_address, nr_of_registers) {
+    this->reading.type = type;
+    this->name = name;
+
+    xTaskCreate(mb_write_task, name.c_str(), 256, this, tskIDLE_PRIORITY + 1, &write_task_handle);
+}
+
+void ModbusWriteHandler::mb_write() {
+    uint32_t received_value = 0;
+
+    for (;;) {
+        if (xTaskNotifyWait(0, ULONG_MAX, &received_value, portMAX_DELAY) == pdTRUE) {
+            while (controller->isbusy())
+                vTaskDelay(5);
+            reg.start_transfer(received_value);
+        }
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -24,57 +24,45 @@ uint32_t read_runtime_ctr(void) { return timer_hw->timerawl; }
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
 
-// void read_old(void *pvParameters) {
-//     auto reg = static_cast<ReadRegister *>(pvParameters);
-//     while (true) {
-//         reg->start_transfer();
-//         while (mbctrl->isbusy())
-//             vTaskDelay(10); // IMPORTANT if modbus controller is busy it means the transfers are
-//             not
-//                             // compeleted yet
-//         std::cout << reg->get_float() << std::endl;
-//         vTaskDelay(1000);
-//     }
-// }
+void init_modbus_handlers(SubscriptionManager &manager, shared_modbus mbctrl) {
+
+}
 
 int main() {
     stdio_init_all();
     std::cout << "Booting" << std::endl;
 
-    shared_uart u{std::make_shared<Uart_instance>(1, 9600, UART_TX_PIN, UART_RX_PIN,
+    shared_uart uart_i{std::make_shared<Uart_instance>(1, 9600, UART_TX_PIN, UART_RX_PIN,
                                                   2)}; // 1 for testbox 2 for fullscale
-    shared_modbus mbctrl{std::make_shared<ModbusCtrl>(u)};
+    shared_modbus mbctrl{std::make_shared<ModbusCtrl>(uart_i)};
 
-    auto co2_handler = std::make_shared<ModbusReadHandler>(mbctrl, 240, 0x0, 2, true, ReadingType::CO2, "co2");
-    auto temp_handler = std::make_shared<ModbusReadHandler>(mbctrl, 241, 0x2, 2, true, ReadingType::TEMPERATURE, "temp");
-    auto rh_handler = std::make_shared<ModbusReadHandler>(mbctrl, 241, 0x0, 2, true, ReadingType::REL_HUMIDITY, "rh");
-    auto fcounter_handler = std::make_shared<ModbusReadHandler>(mbctrl, 1, 0x4, 2, true, ReadingType::FAN_COUNTER, "fcounter");
-
-    SubscriptionManager sm;
+    SubscriptionManager manager;
     TestSubscriber eeprom("EEPROM");
     TestSubscriber display("Display");
+    //TestWriter writer("Controller");
 
-    sm.add_register_handler(ReadingType::CO2, co2_handler);
-    sm.add_register_handler(ReadingType::TEMPERATURE, temp_handler);
+    //init_modbus_handlers(manager, mbctrl);
 
-    sm.subscribe_to_handler(ReadingType::CO2, eeprom.get_queue_handle());
-    sm.subscribe_to_handler(ReadingType::TEMPERATURE, eeprom.get_queue_handle());
-    sm.subscribe_to_handler(ReadingType::CO2, display.get_queue_handle());
-    sm.subscribe_to_handler(ReadingType::TEMPERATURE, display.get_queue_handle());
+    ReadRegister co2_register{mbctrl, 240, 0x0, 2};
+    ReadRegister temp_register{mbctrl, 241, 0x2, 2};
 
-    // ReadRegister co2(mbctrl, 240, 0x0, 2);
-    // ReadRegister temp(mbctrl, 241, 0x2, 2);
+    auto co2 = std::make_shared<ModbusReadHandler>(co2_register, ReadingType::CO2, "co2");
+    auto temp = std::make_shared<ModbusReadHandler>(temp_register, ReadingType::TEMPERATURE, "temp");
+    //auto rh = std::make_shared<ModbusReadHandler>(mbctrl, 241, 0x0, 2, true, ReadingType::REL_HUMIDITY, "rh");
+    //auto fcount = std::make_shared<ModbusReadHandler>(mbctrl, 1, 0x4, 2, true, ReadingType::FAN_COUNTER, "fcounter");
 
-    // xTaskCreate(read_old, "read_co2", 256, &co2, tskIDLE_PRIORITY + 1, nullptr);
-    // xTaskCreate(read_old, "read_temp", 256, &temp, tskIDLE_PRIORITY + 1, nullptr);
+    manager.add_register_handler(ReadingType::CO2, co2);
+    manager.add_register_handler(ReadingType::TEMPERATURE, temp);
+    //manager.add_register_handler(ReadingType::REL_HUMIDITY, rh);
+    //manager.add_register_handler(ReadingType::FAN_COUNTER, fcount);
 
-    // xTaskCreate(read_task, "read_task", 256, (void *) &co2, tskIDLE_PRIORITY + 1, nullptr);
-
-    // xTaskCreate(read_task_16, "read_task_16", 256, (void *) &co, tskIDLE_PRIORITY + 1, nullptr);
+    manager.subscribe_to_handler(ReadingType::CO2, eeprom.get_queue_handle());
+    manager.subscribe_to_handler(ReadingType::TEMPERATURE, eeprom.get_queue_handle());
+    manager.subscribe_to_handler(ReadingType::CO2, display.get_queue_handle());
+    manager.subscribe_to_handler(ReadingType::TEMPERATURE, display.get_queue_handle());
 
     vTaskStartScheduler();
 
     for (;;) {
     }
 }
-
