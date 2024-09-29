45cc80077891805fd90099890e9cfd62cae2cede
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-09-30 02:53:02 +0300

    Create task_defines.h

---
+++ src/task_defines.h
@@ -0,0 +1,14 @@
+#ifndef TASK_DEFINES_H
+#define TASK_DEFINES_H
+
+#include "FreeRTOS.h"
+
+enum TaskPriority {
+    TASK_PRIORITY_IDLE = (tskIDLE_PRIORITY),
+    TASK_PRIORITY_LOW = (tskIDLE_PRIORITY + 1),
+    TASK_PRIORITY_MEDIUM = (tskIDLE_PRIORITY + 2),
+    TASK_PRIORITY_HIGH = (tskIDLE_PRIORITY + 3),
+    TASK_PRIORITY_ABSOLUTE = (tskIDLE_PRIORITY + 4)
+};
+
+#endif
