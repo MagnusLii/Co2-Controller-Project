2c00a72bb2e05013b7cde59b022fb39bc260236a
Ronald5607 <roni.paloniemi@hotmail.com>
2024-10-05 13:50:12 +0300

    screen

--- inc/display/screen.hpp
+++ inc/display/screen.hpp
@@ -10,16 +10,19 @@
 class Screen {
     public:
         Screen(std::shared_ptr<PicoI2C> i2c, uint16_t device_address = 0x3C, uint16_t width = 128, uint16_t height = 64);
-        QueueHandle_t get_queue_handle(void);
+        QueueHandle_t get_control_queue_handle(void);
+        QueueHandle_t get_reading_queue_handle(void);
     private:
         static void screen_task(void *pvParameters);
+        void set_target_task(void *pvParameters);
         void set_static_shapes(void);
         void set_reading_value(Reading &reading);
-        void set_manual_fan_speed(uint16_t percentage);
+        void set_bar(uint16_t percentage);
         std::unique_ptr<ssd1306os> display;
         QueueHandle_t control_queue;
+        QueueHandle_t reading_queue;
         mono_vlsb reading_blit_buf;
-        mono_vlsb manual_fanspeed_buf;
+        mono_vlsb bar_buf;
 };
 
 #endif
