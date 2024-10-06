c1c9d89c78d2c9dcb188f8dfe93df36a71ffbf2e
kkoskivi <elmeri.koskivirta@gmail.com>
2024-10-06 20:56:23 +0300

    Eeprom should be able to be read and written to.
    Sponsored by RED BULL GmbH

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ void setup_task(void *pvParameters) {
     params->logger = std::make_shared<Logger>(params->i2c_0);
     params->modbus = std::make_shared<ModbusCtrl>(params->uart);
     params->registry = std::make_shared<DeviceRegistry>(params->modbus, params->i2c_1);
-    params->registry->set_initial_values(params->logger->co2_target, params->logger->fan_speed, false);
+    params->registry->set_initial_values(params->logger->co2_target_return, params->logger->fan_speed_return, false);
 
     params->screen = std::make_shared<Screen>(params->i2c_1);
     params->rotary = std::make_shared<Rotary>();
